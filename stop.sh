d216d0d7fbde0abd40313b51b92d15d761776932
moti <moti.kor@gmail.com>
2018-04-28 09:53:28 +0700

    fix stop \n

--- stop.sh
+++ stop.sh
@@ -6,5 +6,6 @@ if [[ "$STATUS" == "STOP" ]] ; then
 fi
 line=`grep -n "####" current.log | tail -n 1 | egrep -oh "^\d+"`
 interims_sum=$(sed -n $line,\$p current.log | grep PAUSE | cut -d'|' -f4|./sum_hours.sh)
-sum=$(echo "$interims_sum\n`./sum_last.sh`" | ./sum_hours.sh)
+sum=$(echo -e "${interims_sum}\n$(./sum_last.sh)" | ./sum_hours.sh)
+echo $sum
 echo "STOP|`date +"%Y-%m-%d %H:%M|%s"`|$sum| $1 " >> current.log
